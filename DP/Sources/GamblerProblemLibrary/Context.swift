3b200b68d1935f47018c9d58c7b1cade9af8695d
Jianwei Xie <xiejw0217@gmail.com>
2019-05-27 18:01:46 -0700

    Add comment.

--- DP/Sources/GamblerProblemLibrary/Context.swift
+++ DP/Sources/GamblerProblemLibrary/Context.swift
@@ -1,5 +1,8 @@
 public struct Context {
+    /// Total number of states in the Gambler Problem model.
     public let stateCount: Int
+
+    /// The maximum value to be achievd, i.e., the goal.
     public let maxValue: Int
 
     public init(stateCount: Int) {
